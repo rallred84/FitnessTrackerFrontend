e98a9d10260a51c41e1a0c852180f4ea83305e05
Robert Allred <rallred84@gmail.com>
2023-06-15 23:49:01 -0500

    Added Create Routine function to Routines Page

--- src/components/routines/routines.jsx
+++ src/components/routines/routines.jsx
@@ -4,9 +4,14 @@ import './routines.css';
 import { useNavigate, useOutletContext } from 'react-router';
 
 const Routines = () => {
-  const { allRoutines, setAllRoutines, myProfile } = useOutletContext();
+  const { allRoutines, setAllRoutines, myProfile, token } = useOutletContext();
 
   const [createWindowOpen, setCreateWindowOpen] = useState(false);
+
+  const [routineName, setRoutineName] = useState('');
+  const [routineGoal, setRoutineGoal] = useState('');
+  const [routineIsPublic, setRoutineIsPublic] = useState(true);
+  const [createRoutineError, setCreateRoutineError] = useState('');
 
   const navigate = useNavigate();
 
@@ -23,36 +28,103 @@ const Routines = () => {
     navigate(`/routines/${routine.id}`);
   };
 
+  const handleRoutineCreate = async (e) => {
+    e.preventDefault();
+    const newRoutine = await createRoutine(
+      token,
+      routineName,
+      routineGoal,
+      routineIsPublic
+    );
+
+    console.log(newRoutine);
+
+    if (!newRoutine.id) {
+      if (
+        newRoutine.error ===
+        'duplicate key value violates unique constraint "routines_name_key"'
+      ) {
+        setCreateRoutineError('A routine with this name already exists');
+      } else setCreateRoutineError(newRoutine.error);
+      return;
+    }
+    const allRoutines = await getAllRoutines();
+    setAllRoutines(allRoutines);
+
+    navigate(`/routines/${newRoutine.id}`);
+  };
+
   return (
     <>
-      {myProfile.id && (
-        <div id="routines-header">
-          <div>
-            <p>Returning {allRoutines.length} routines...</p>
-            <div id="routine-search">
-              <form action="filter-routines">
-                <input
-                  id="filter-routines-search"
-                  type="text"
-                  placeholder="Enter a Keyword to narrow your routine search"
-                />
-                <button id="filter-routines-button">Filter Routines</button>
-              </form>
-            </div>
-          </div>
-          <div>
-            <button id="open-create-routine">Create New Routine</button>
+      <div id="routines-header">
+        <div>
+          <p>Returning {allRoutines.length} routines...</p>
+          <div id="routine-search">
+            <form action="filter-routines">
+              <input
+                id="filter-routines-search"
+                type="text"
+                placeholder="Enter a Keyword to narrow your routine search"
+              />
+              <button id="filter-routines-button">Filter Routines</button>
+            </form>
           </div>
         </div>
-      )}
+        {myProfile.id && (
+          <div>
+            {!createWindowOpen ? (
+              <button
+                id="open-close-create-routine"
+                onClick={() => setCreateWindowOpen(true)}
+              >
+                Create New Routine
+              </button>
+            ) : (
+              <button
+                id="open-close-create-routine"
+                onClick={() => setCreateWindowOpen(false)}
+              >
+                Cancel Create Routine
+              </button>
+            )}
+          </div>
+        )}
+      </div>
 
-      {!createWindowOpen && (
-        <div id="create-post-form">
+      {createWindowOpen && (
+        <div id="create-routine-form">
           <h3>Create Your Routine</h3>
-          <form action="create-post">
-            <input type="text" placeholder="Routine Name" />
-            <input type="text" placeholder="Routine Goal" />
+          <form
+            action="create-post"
+            id="create-routine-form-inputs"
+            onSubmit={(e) => handleRoutineCreate(e)}
+          >
+            <input
+              type="text"
+              placeholder="Routine Name"
+              value={routineName}
+              onChange={(e) => setRoutineName(e.target.value)}
+            />
+            <textarea
+              type="text"
+              placeholder="Routine Goal"
+              value={routineGoal}
+              onChange={(e) => setRoutineGoal(e.target.value)}
+            />
+            <div>
+              <label htmlFor="isPublic">Is Public?</label>
+              <input
+                type="checkbox"
+                name="isPublic"
+                defaultChecked
+                onChange={(e) => {
+                  setRoutineIsPublic(e.target.checked);
+                }}
+              />
+            </div>
+            <button id="create-routine-button">Create!!</button>
           </form>
+          {createRoutineError}
         </div>
       )}
 
